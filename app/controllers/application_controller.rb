0726f88268db4502fd21d242c5cabe0178934b10
miriwo0104 <shunokawa@gmail.com>
2020-02-06 16:45:09 +0900

    ログイン中のユーザ情報を取得する処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+    def set_current_user
+        @current_user = User.find_by(id: session[:user_id])
+        @today = Time.current.to_s.slice(0..10)
+    end
 end
