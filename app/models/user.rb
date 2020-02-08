86d212f22df8a124546593e69ec56b2bc76be5e1
miriwo0104 <shunokawa@gmail.com>
2020-02-08 12:47:53 +0900

    バリデーション設定の閉じ中括弧の位置を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
     #emailが空、重複、フォーマットが正規表現通りではないとはじく
     validates :email, {presence: true, 
                        uniqueness: true, 
-                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}}
+                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    }
+    
     #nameが空の時はじく
-    validates :name, {presence: true}
+    validates :name, {presence: true,
+    }
 end
