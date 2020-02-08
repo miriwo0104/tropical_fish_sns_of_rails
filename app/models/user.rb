f1ddafe5ed77e18921d62562f4e81c4bfa62869b
miriwo0104 <shunokawa@gmail.com>
2020-02-08 12:38:11 +0900

    usersテーブルにバリデーションを設定した
    emailが空、重複している、フォーマットが正規表現通りではない時にはじく
    nameが空の時にはじく

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
+    #emailが空、重複、フォーマットが正規表現通りではないとはじく
+    validates :email, {presence: true, 
+                       uniqueness: true, 
+                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}}
+    #nameが空の時はじく
+    validates :name, {presence: true}
 end
