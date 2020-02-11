43ad6850541660a17faec65f3d56acfc0d67f06e
miriwo0104 <shunokawa@gmail.com>
2020-02-11 11:35:37 +0900

    addressesテーブルのaddressカラムにバリデーションをかけた
    空の登録をはじく
    すでに登録されているデータと被らない
    フォーマット通りの入力

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,6 @@
 class Address < ApplicationRecord
+    validates :address, {presence: true, 
+                        uniqueness: true, 
+                        format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    }
 end
