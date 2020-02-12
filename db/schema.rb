d366c00d1dbda0a14b70150b1be89ec5563becb1
miriwo0104 <shunokawa@gmail.com>
2020-02-12 11:25:21 +0900

    postsテーブルにimgカラムをstirng型で追加した
    マイグレート直後

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_102812) do
+ActiveRecord::Schema.define(version: 2020_02_12_022321) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "random_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_02_09_102812) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "link"
+    t.string "img"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
