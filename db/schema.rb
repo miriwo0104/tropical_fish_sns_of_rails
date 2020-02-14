9d451339ae2f817accae82c3054eaa0e393cf4e8
miriwo0104 <shunokawa@gmail.com>
2020-02-14 14:49:44 +0900

    dataデーブルにuser_idのカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_014400) do
+ActiveRecord::Schema.define(version: 2020_02_14_021536) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "random_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_02_14_014400) do
     t.string "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_id"
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
