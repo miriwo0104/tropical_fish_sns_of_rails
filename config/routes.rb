f1bdb67444ad97e249293789f2606c3a9baf0505
miriwo0104 <shunokawa@gmail.com>
2020-02-18 15:09:08 +0900

    カレンダーページの表示に必要な一式を追加した
    ルーティングに記載を実施
    コントローラを設置
    ビューを設置
    ヘッダにカレンダーページへのリンクを記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'users/new'
+  get "/:id/calendar" => "calendar#index"
+  get "/posts/index" => "posts#index"
+  get "/users/new" => "users#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/" => "home#top"
   get "/about" => "home#about"
